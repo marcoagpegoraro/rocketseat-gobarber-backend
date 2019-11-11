efc832e827ccaa3f27a8741a343b4b28c1daddb3
Marco Antonio Gonçalves Pegoraro <magpegoraro@ibm.com>
2019-11-11 14:31:14 -0300

    Criar appointment funcionando.

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController'
 import authMiddleware from './app/middlewares/auth'
 import FileController from './app/controllers/FileController'
 import ProviderController from './app/controllers/ProviderController'
+import AppointmentController from './app/controllers/AppointmentController'
 
 const routes = new Router()
 const upload = Multer(multerConfig)
@@ -20,6 +21,8 @@ routes.use(authMiddleware)
 routes.put('/users', UserController.update)
 
 routes.get('/providers', ProviderController.index)
+
+routes.post('/appointments', AppointmentController.store)
 
 routes.post('/files', upload.single('file'), FileController.store)
 
