8ab1313611464a1c733bc70e4ce91c53d12146e9
Marco Antonio Gonçalves Pegoraro <magpegoraro@ibm.com>
2019-11-12 11:40:23 -0300

    13 - Marcar notificações como lidas

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -4,7 +4,7 @@ import Notification from "../schemas/Notification"
 class NotificationController {
   async index(req, res) {
     const checkIsProvider = await User.findOne({
-      where: {id: req.userId, provider: true}
+      where: { id: req.userId, provider: true }
     })
 
     if (!checkIsProvider) {
@@ -13,9 +13,18 @@ class NotificationController {
 
     const notifications = await Notification.find({
       user: req.userId,
-    }).sort({createdAt: 'desc'}).limit(20)
+    }).sort({ createdAt: 'desc' }).limit(20)
 
     return res.json(notifications)
+  }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id,
+      { read: true },
+      { new: true }
+    )
+    return res.json(notification)
   }
 }
 
